da9c9191ce68d3ef772c2c835856fa6e619331e3
Daniel Flores <daniel_f94@outlook.com>
2024-05-05 21:50:01 -0600

    finalizando curso basico de JS

--- JS/app.js
+++ JS/app.js
@@ -401,3 +401,27 @@ async function app(){
 
 app();
 
+async function obtenerEmpleado(){
+    const archivo = 'empleados.json'
+
+    // Con promises
+    // fetch(archivo)
+    //     .then( resultado => {
+    //         return resultado.json()
+    //     })
+    //     .then( datos => {
+    //         const {empleados} = datos;
+
+    //         empleados.forEach( empleado => {
+    //             console.log(empleado.id)
+    //             console.log(empleado.nombre)
+    //             console.log(empleado.puesto)
+    //         })
+    //     })
+
+    const resultado = await fetch(archivo)
+    const datos = await resultado.json()
+    console.log(datos)
+}
+
+obtenerEmpleado()
